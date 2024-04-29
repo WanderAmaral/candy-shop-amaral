e1ed6e2f01cb1a75d160d8b07c56a5d883fa5ee2
Wander Amaral <wanderamaral013@gmail.com>
2024-04-29 16:53:03 -0300

    feat: ensure auth form page register

--- src/app/register/_components/create-account.tsx
+++ src/app/register/_components/create-account.tsx
@@ -1,4 +1,6 @@
+"use client";
 import { Button } from "@/components/ui/button";
+import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import {
@@ -9,54 +11,134 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const formSchema = z.object({
+  name: z.string(),
+  email: z.string(),
+  password: z.string(),
+  typeUser: z.string(),
+});
 
 const CreateAcount = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  const handleOnSubmit = (data: z.infer<typeof formSchema>) => {
+    console.log(data);
+  };
+
   return (
     <>
-      <div className="h-screen  mx-auto max-w-[360px] mt-10">
-        <div className="flex flex-col w-full gap-5">
-          <div className=" text-center flex flex-col gap-4">
-            <h1 className="font-semibold text-3xl capitalize">Criar Conta</h1>
-            <p className="text-lg">Ainda não tem uma conta? Registre-se.</p>
-          </div>
-          <Label className="text-xl font-semibold">Nome</Label>
-          <Input
-            placeholder="Jhon Doe"
-            className="border-zinc-300 border-b-2"
-          />
-          <Label className="text-xl font-semibold">Email</Label>
-          <Input
-            placeholder="hello@reallygreatsite.com"
-            className="border-zinc-300 border-b-2"
-          />
-          <Label className="text-xl font-semibold">Senha</Label>
-          <Input
-            type="password"
-            className="border-zinc-300 border-b-2"
-            placeholder="**********"
-          />
-          <Label className=" uppercase text-lg font-semibold">
-            Empresa/Cliente
-          </Label>
-          <Select>
-            <SelectTrigger>
-              <SelectValue placeholder="Empresa/Cliente" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectGroup>
-                <SelectItem value="company">Empresa</SelectItem>
-                <SelectItem value="client">Cliente</SelectItem>
-              </SelectGroup>
-            </SelectContent>
-          </Select>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(handleOnSubmit)}>
+          <div className="h-screen  mx-auto max-w-[360px] mt-10">
+            <div className="flex flex-col w-full gap-5">
+              <div className=" text-center flex flex-col gap-4">
+                <h1 className="font-semibold text-3xl capitalize">
+                  Criar Conta
+                </h1>
+                <p className="text-lg">Ainda não tem uma conta? Registre-se.</p>
+              </div>
+              <Label className="text-xl font-semibold">Nome</Label>
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormControl>
+                      <Input
+                        placeholder="Jhon Doe"
+                        className="border-zinc-300 border-b-2"
+                        {...field}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <Label className="text-xl font-semibold">Email</Label>
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormControl>
+                      <Input
+                        placeholder="hello@reallygreatsite.com"
+                        className="border-zinc-300 border-b-2"
+                        {...field}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <Label className="text-xl font-semibold">Senha</Label>
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormControl>
+                      <Input
+                        type="password"
+                        className="border-zinc-300 border-b-2"
+                        placeholder="**********"
+                        {...field}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
 
-          <div className="flex flex-col mt-8">
-            <Button className=" bg-color-dark text-xl hover:bg-color-darker h-14 rounded-2xl">
-              Criar Conta
-            </Button>
+              <Label className=" uppercase text-lg font-semibold">
+                Empresa/Cliente
+              </Label>
+
+              <FormField
+                control={form.control}
+                name="typeUser"
+                render={({ field }) => (
+                  <FormItem>
+                    <Select
+                      onValueChange={field.onChange}
+                      defaultValue={field.value}
+                    >
+                      <FormControl>
+                        <SelectTrigger>
+                          <SelectValue placeholder="Empresa/Cliente" />
+                        </SelectTrigger>
+                      </FormControl>
+                      <SelectContent>
+                        <SelectGroup>
+                          <SelectItem value="company">Empresa</SelectItem>
+                          <SelectItem value="client">Cliente</SelectItem>
+                        </SelectGroup>
+                      </SelectContent>
+                    </Select>
+                  </FormItem>
+                )}
+              />
+
+              <div className="flex flex-col mt-8">
+                <Button
+                  type="submit"
+                  className=" bg-color-dark text-xl hover:bg-color-darker h-14 rounded-2xl"
+                >
+                  Criar Conta
+                </Button>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
+        </form>
+      </Form>
     </>
   );
 };
