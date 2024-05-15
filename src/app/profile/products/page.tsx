5937396ce8513f21cdaaf3dce0a7769ba7ec0de8
Wander Amaral <wanderamaral013@gmail.com>
2024-05-15 14:44:01 -0300

    commit

--- src/app/profile/products/page.tsx
+++ src/app/profile/products/page.tsx
@@ -1,7 +1,12 @@
 import CreateCompanyForm from "./_components/create-company";
 
-const ProductsCompany =  () => {
-  return <CreateCompanyForm />;
+const ProductsCompany = () => {
+  return (
+    <div className=" flex justify-center items-center flex-col gap-10 pt-20">
+      <h1>Cria Agora sua emresa!</h1>
+      <CreateCompanyForm />
+    </div>
+  );
 };
 
 export default ProductsCompany;
