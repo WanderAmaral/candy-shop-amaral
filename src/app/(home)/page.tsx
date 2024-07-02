0c2aa47a66db2ea3c93fc87b5e4b6c7a6805a013
Wander Amaral <wanderamaral013@gmail.com>
2024-07-02 01:31:33 -0300

    fix: changed name and properties in home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,39 +1,37 @@
 import CandyProduct from "@/components/product-item";
 import { prisma } from "../_modules/services/database/prisma";
-import ProductCard from "@/components/product-card";
+import ProductCardUser from "@/components/product-card-user";
 
 export default async function Home() {
-  const company = await prisma.company.findMany({
+  const companies = await prisma.company.findMany({
     include: {
       products: true,
     },
   });
-  const userProduct = await prisma.user.findMany({
+  const users = await prisma.user.findMany({
     include: { products: true },
   });
 
   return (
-    <div className=" bg-color-lighter">
+    <div className="bg-color-lighter">
       <div className="p-5">
-        {userProduct.map((user) => (
+        {users.map((user) => (
           <div key={user.id}>
             {user.products.length > 0 ? (
               <>
-                <span className="italic text-xl">Empresa: </span> <span className="text-xl font-semibold pb-2">{user.name}</span>
+                <span className="italic text-xl">Empresa: </span>
+                <span className="text-xl font-semibold pb-2">{user.name}</span>
                 <div className="flex gap-3 overflow-x-auto">
                   {user.products.map((product) => (
-                    <ProductCard product={product} key={product.id} />
+                    <ProductCardUser user={{ ...user, products: [product] }} key={product.id} />
                   ))}
                 </div>
               </>
-            ) : (
-              <></>
-            )}
-
+            ) : null}
           </div>
         ))}
       </div>
-      {company.map((company) => (
+      {companies.map((company) => (
         <CandyProduct company={company} key={company.id} />
       ))}
     </div>
