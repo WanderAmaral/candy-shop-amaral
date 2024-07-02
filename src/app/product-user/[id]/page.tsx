7d5fbf8008d134bebf568a36167d8eaa15335f09
Wander Amaral <wanderamaral013@gmail.com>
2024-07-02 15:06:47 -0300

    commit

--- src/app/product-user/[id]/page.tsx
+++ src/app/product-user/[id]/page.tsx
@@ -1,6 +1,8 @@
 import { prisma } from "@/app/_modules/services/database/prisma";
 import ProductInfoUser from "./_components/product-info-user";
 import { Card } from "@/components/ui/card";
+import ProductCard from "@/components/product-card";
+import { CompanyWithProducts } from "@/app/product/types/product-type";
 
 interface ProductDetailsUserPageProps {
   params: {
@@ -21,8 +23,20 @@ const ProductDetailsUserPage = async ({
   });
 
   if (!product) {
-    return <div>Product not found</div>;
+    return null;
   }
+
+
+  // const company = (await prisma.company.findUnique({
+  //   where: {
+  //     id: product?.companyId,
+  //   },
+  //   include: { products: true },
+  // })) as CompanyWithProducts;
+
+  // if (!product) {
+  //   return null;
+  // }
 
   return (
     <div className="py-6 px-16">
